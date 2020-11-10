ec96fe2aa614740b70a06856187bd024a30aef20
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-11-10 18:08:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,16 @@ setup(
     url='github.com/roshanlam/PyCT',
     license='',
     author='roshanlam',
-    description='A library for colored text'
+    description='A library for colored text',
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
 )
