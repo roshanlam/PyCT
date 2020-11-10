3adb9b8a4b1bb17938a885606f2a4b0ba72ecdfd
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-11-10 17:57:39 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,6 @@
+from PyColorText import PyCT as ct
+
+print(ct("*", "red")*15)
+
+name = "Roshan"
+print(ct("Hello, my name is {}!".format(name), "blue"))
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='PyColorText',
+    version='0.0.1',
+    packages=['PyColorText'],
+    url='github.com/roshanlam/PyCT',
+    license='',
+    author='roshanlam',
+    description='A library for colored text'
+)
